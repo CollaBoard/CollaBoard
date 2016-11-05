7e13bd52a3bab00367804dae32545cae22de5d15
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-05 14:04:53 -0500

    Setup database connection for server

---
+++ server/lib/knex-driver.js
@@ -0,0 +1,17 @@
+// Read configuration file
+const config = require('../../knexfile.js');
+
+// Configure knex with the correct environment configuration
+const env = process.env.NODE_ENV || 'development';
+const db = require('knex')(config[env]);
+
+// Export the db object, which will be able to make database connections
+module.exports = db;
+
+// Function for your testing suite
+db.deleteEverything = function deleteEverything() {
+  if (env !== 'test') return Promise.reject();
+  // TODO: Delete data from all tables (useful for testing)
+  // return db('users').truncate()
+  return Promise.reject();
+};
