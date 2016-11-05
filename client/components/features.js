7ecac0982fb605627863f9a66c8c534f5c393bd1
Kendrick Gardner <kbgar24@gmail.com>
2016-11-05 13:09:31 -0500

    Update CSS file structure. Add Features CSS

--- client/components/features.js
+++ client/components/features.js
@@ -3,6 +3,28 @@ import React from 'react';
 class Features extends React.Component {
 	render() {
 		return (
+			<div>
+				<h3 className="features-title">
+					Start getting more done in less time
+				</h3>
+				<p className="features-subtitle"> Manage taska nd projects anywhere with CollaBoard. At home. At school. At work. And on virtually any device </p>
+				<div className="productivity-icons">  
+					
+					<div className="icon-access-anywhere"> 
+						<img src="../img/cloud-icon.png"/>
+					</div>
+					
+					<div className="icon-collab">
+						<img src="../img/cloud-icon.png"/>
+					</div>
+					
+					<div className="icon-distraction-free">
+						<img src="../img/cloud-icon.png"/>
+					</div>
+				
+				</div>
+			</div>
+
 		);
 	}
 }
