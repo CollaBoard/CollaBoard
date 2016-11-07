ae9d842e89856bac588d943167906860ac6a95be
Michael Lefkowitz <leftynaut@users.noreply.github.com>
2016-11-07 16:28:40 -0600

    Fix scrolling feat23 (#25)
    * drawing with mouse works but click sends line too
    
    * works on desktop
    
    * works on mobile
    
    * improved variable names
    
    * refactored
    
    * morning fix
    
    * works with tap logo temp fix
    
    * cut sidenav in half
    
    * fixed issues as req by shamsup
    
    * moved x and y vars to drawtouch and drawmouse scope
    
    * xor
    
    * removed xor
    
    * moved functions into createCanvas function so canvas and content variables are accessible

--- client/public/js/whiteboard.js
+++ client/public/js/whiteboard.js
@@ -1,66 +1,88 @@
-let ctx, color = '#000';
-
-document.addEventListener('DOMContentLoaded', () => {
-  newCanvas()
-}, false );
-
-const newCanvas = function(){
-  let canvas = '<canvas id="canvas" width="635" height="720"></canvas>';
-  let canvasClone = '<canvas id="canvasClone" width="635" height="720"></canvas>';
-  // let canvas = `<canvas id="canvas" width="${window.innerWidth}" height="${window.innerHeight - 90}"></canvas>`;
-	document.getElementById('content').innerHTML = canvas + canvasClone;
-
-	ctx = document.getElementById('canvas').getContext('2d');
-	ctx.strokeStyle = color;
-	ctx.lineWidth = 5;
-
-  drawTouch();
-	drawMouse();
+let canvasState = true;
+const moveCanvas = function() {
+  canvasState = !canvasState;
 }
 
-const drawTouch = function() {
-	let start = function(e) {
-		ctx.beginPath();
-		x = e.changedTouches[0].pageX;
-		y = e.changedTouches[0].pageY - 34;
-		ctx.moveTo(x, y);
-	};
-	let move = function(e) {
-		e.preventDefault();
-		x = e.changedTouches[0].pageX;
-		y = e.changedTouches[0].pageY - 34;
-		ctx.lineTo(x, y);
-		ctx.stroke();
-	};
-  document.getElementById('canvas').addEventListener('touchstart', start, false);
-	document.getElementById('canvas').addEventListener('touchmove', move, false);
-};
+document.addEventListener('DOMContentLoaded', () => {
+  createCanvas()
+}, false );
 
-const drawMouse = function() {
-	let clicked = false;
-	let start = function(e) {
-		clicked = true;
-		ctx.beginPath();
-		x = e.pageX;
-		y = e.pageY - 34;
-		ctx.moveTo(x, y);
-	};
-	let move = function(e) {
-    if(e.shiftKey && clicked) {
-      x = e.pageX;
-      ctx.lineTo(x, y);
-      ctx.stroke();
-    } else if (clicked){
-			x = e.pageX;
-			y = e.pageY - 34;
-			ctx.lineTo(x, y);
-			ctx.stroke();
-		}
-	};
-	let stop = function(e) {
-		clicked = false;
-	};
-  document.getElementById('canvas').addEventListener('mousedown', start, false);
-	document.getElementById('canvas').addEventListener('mousemove', move, false);
-	document.addEventListener('mouseup', stop, false);
-};
+const createCanvas = function(){
+  let ctx, color = '#000';
+  const content = document.getElementById('content');
+  const newCanvas = '<canvas id="canvas" width="1270" height="720"></canvas>';
+  // let canvasClone = '<canvas id="canvasClone" width="635" height="720"></canvas>';
+  // let canvas = `<canvas id="canvas" width="${window.innerWidth}" height="${window.innerHeight - 90}"></canvas>`;
+  // document.getElementById('content').innerHTML = canvas + canvasClone;
+  content.innerHTML = newCanvas;
+  const canvas = document.getElementById('canvas');
+
+  ctx = canvas.getContext('2d');
+  ctx.strokeStyle = color;
+  ctx.lineWidth = 5;
+
+  const drawTouch = function() {
+    let x, y;
+
+    const startTouch = function(e) {
+      ctx.beginPath();
+      x = e.changedTouches[0].pageX + content.scrollLeft;
+      y = e.changedTouches[0].pageY - 34 + content.scrollTop;
+      ctx.moveTo(x, y);
+    };
+    const moveTouch = function(e) {
+      if (canvasState) {
+        e.preventDefault();
+        x = e.changedTouches[0].pageX + content.scrollLeft;
+        y = e.changedTouches[0].pageY - 34 + content.scrollTop;
+        ctx.lineTo(x, y);
+        ctx.stroke();
+      }
+    };
+
+    canvas.addEventListener('touchstart', startTouch, false);
+    canvas.addEventListener('touchmove', moveTouch, false);
+  };
+
+  const drawMouse = function() {
+    let x, y, clicked = false;
+    const startMouse = function(e) {
+      clicked = true;
+      ctx.beginPath();
+      x = e.pageX + content.scrollLeft;
+      y = e.pageY - 34 + content.scrollTop;
+      ctx.moveTo(x, y);
+    };
+    const moveMouse = function(e) {
+      if(e.shiftKey && clicked) {
+        x = e.pageX + content.scrollLeft;
+        ctx.lineTo(x, y);
+        ctx.stroke();
+      } else if (clicked){
+        x = e.pageX + content.scrollLeft;
+        y = e.pageY - 34 + content.scrollTop;
+        ctx.lineTo(x, y);
+        ctx.stroke();
+      }
+    };
+    const stopMouse = function(e) {
+      clicked = false;
+    };
+    const keyCall = function(e) {
+      const key = e.keyCode;
+      // if user hits backspace, undo last stroke
+      if (key === 8) {
+        e.preventDefault()
+        // implement undo function here
+      }
+    };
+    canvas.addEventListener('mousedown', startMouse, false);
+    canvas.addEventListener('mousemove', moveMouse, false);
+    canvas.addEventListener('mouseleave', stopMouse, false);
+    document.addEventListener('mouseup', stopMouse, false);
+    document.addEventListener('keyup', keyCall, false);
+  };
+
+  drawTouch();
+  drawMouse();
+}
