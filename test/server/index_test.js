a2cd5897960ee2a4843bdfea90489a5cb377cd48
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-08 17:26:50 -0600

    Correct linting errors in test files

--- test/server/index_test.js
+++ test/server/index_test.js
@@ -1,25 +1,20 @@
-require(TEST_HELPER) // <--- This must be at the top of every test file.
+require(TEST_HELPER); // <--- This must be at the top of every test file.
 
-var request = require('supertest-as-promised')
-var routes = require(__server + '/index.js')
+const request = require('supertest-as-promised');
 
-describe("The Server", function() {
+const routes = require(`${__server}/index.js`);
 
-  var app = TestHelper.createApp()
-  app.use('/', routes)
-  app.testReady()
+describe('The Server', () => {
+  const app = TestHelper.createApp();
+  app.use('/', routes);
+  app.testReady();
 
-  it_("serves an example endpoint", function * () {
-
-    //
-    // Notice how we're in a generator function (indicated by the the *)
-    // See test/test-helper.js for details of why this works.
-    //
+  it_('serves an example endpoint', function* () {
     yield request(app)
       .get('/api/tags-example')
       .expect(200)
-      .expect(function(response) {
-        expect(response.body).to.include('node')
-      })
-  })
-})
+      .expect((response) => {
+        expect(response.body).to.include('node');
+      });
+  });
+});
--- test/test-helper.js
+++ test/test-helper.js
@@ -1,60 +1,60 @@
-process.env.NODE_ENV = 'test'
+process.env.NODE_ENV = 'test';
 
+const bodyParser = require('body-parser');
 // The following allows you to require files independent of
 // the location of your test file.
 // Example:
 //  var User = require(__server + '/models/user.js')
 //
-global.__test   = __dirname
-global.__server = __dirname + '/../server'
-global.__client = __dirname + '/../client'
+global.__test = __dirname;
+global.__server = `${__dirname}/../server`;
+global.__client = `${__dirname}/../client`;
 
 //
 // Assertions
 //
-var chai = require('chai')
-chai.use( require('chai-subset') )
+const chai = require('chai');
+chai.use(require('chai-subset'));
 
 // Option 1: Make the `expect` function available in every test file
-global.expect = chai.expect
+global.expect = chai.expect;
 // Option 2: Make everything should-able
 // global.should = chai.should()
-
 
 //
 // Helper Functions
 //
 // This is the object you can attach any helper functions used across
 // several test files.
-global.TestHelper = {}
+global.TestHelper = {};
 
 //
 // Mock apps for API testing
 //
-var express = require('express')
+const express = require('express');
 
-TestHelper.createApp = function (loader) {
-  var app = express()
-  app.use(require('body-parser').json())
+TestHelper.createApp = function () {
+  const app = express();
+  app.use(bodyParser.json());
 
   app.testReady = function () {
     // Log all errors
-    app.use(function (err, req, res, next) {
-      console.error("==Error==")
-      console.error("   " + err.stack)
-      next(err)
-    })
-  }
-  return app
-}
+    app.use((err, req, res, next) => {
+      console.error('==Error==');
+      console.error(`   ${err.stack}`);
+      next(err);
+    });
+  };
+  return app;
+};
 
 //
 // Mocha "helpers" to support coroutines tests
 //
-var Bluebird = require('bluebird')
+const Bluebird = require('bluebird');
 
-global.before_ = function (f) { before ( Bluebird.coroutine(f) ) }
-global.beforeEach_ = function (f) { beforeEach ( Bluebird.coroutine(f) ) }
-global.it_ = function (description, f) { it ( description, f && Bluebird.coroutine(f) ) }
-global.xit_ = function (description, f) { xit ( description, f ) }
-global.it_.only = function (description, f) { it.only( description, Bluebird.coroutine(f) ) }
+global.before_ = function (f) { before(Bluebird.coroutine(f)); };
+global.beforeEach_ = function (f) { beforeEach(Bluebird.coroutine(f)); };
+global.it_ = function (description, f) { it(description, f && Bluebird.coroutine(f)); };
+global.xit_ = function (description, f) { xit(description, f); };
+global.it_.only = function (description, f) { it.only(description, Bluebird.coroutine(f)); };
