264937f8f7cd6e32ff772e4d0a1ec9954dcbbb85
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-08 16:54:30 -0600

    Correct all linting errors in main component folder

--- client/src/components/main/features.js
+++ client/src/components/main/features.js
@@ -1,35 +1,31 @@
 import React from 'react';
 
-class Features extends React.Component {
-  render() {
-    return (
-      
-      <div className="features-section">
-            <img src="../../../public/assets/img/cloud-icon.png"/>
-        <hr/>
-        <h3 className="features-title">
-          Start getting more done in less time
-        </h3>
-        <p className="features-subtitle"> Manage tasks and projects anywhere with CollaBoard. At home. At school. At work. On virtually any device </p>
-        <div className="productivity-icons">  
-          
-          <div className="icon-access-anywhere"> 
-            <img src="../../../public/assets/img/cloud-icon.png"/>
-          </div>
-          
-          <div className="icon-collab">
-            <img src="../../../public/assets/img/cloud-icon.png"/>
-          </div>
-          
-          <div className="icon-distraction-free">
-            <img src="../../../public/assets/img/cloud-icon.png"/>
-          </div>
-        
-        </div>
+const Features = () => (
+  <div className="features-section">
+    <img src="../../../public/assets/img/cloud-icon.png" alt="cloud" />
+    <hr />
+    <h3 className="features-title">
+      Start getting more done in less time
+    </h3>
+    <p className="features-subtitle">
+      Manage tasks and projects anywhere with CollaBoard.
+      At home. At school. At work. On virtually any device
+    </p>
+    <div className="productivity-icons">
+
+      <div className="icon-access-anywhere">
+        <img src="../../../public/assets/img/cloud-icon.png" alt="cloud" />
       </div>
 
-    );
-  }
-}
+      <div className="icon-collab">
+        <img src="../../../public/assets/img/cloud-icon.png" alt="cloud" />
+      </div>
+
+      <div className="icon-distraction-free">
+        <img src="../../../public/assets/img/cloud-icon.png" alt="cloud" />
+      </div>
+    </div>
+  </div>
+);
 
 export default Features;
--- client/src/components/main/product-select.js
+++ client/src/components/main/product-select.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const ProductSelect = () => {
-	
-	return (
-
-		<div className="product-select-container">
-			<Link to="/whiteboard" className="product-select-link waves-effect waves-light btn-large product-select-link">Whiteboard</Link>
-			<Link to="/texteditor" className="product-select-link waves-effect waves-light btn-large  product-select-link">Text Editor</Link>
-		</div>
-
-	);
-	
-}
+const ProductSelect = () => (
+  <div className="product-select-container">
+    <Link
+      to="/whiteboard"
+      className="product-select-link waves-effect waves-light btn-large"
+    >
+      Whiteboard
+    </Link>
+    <Link
+      to="/texteditor"
+      className="product-select-link waves-effect waves-light btn-large"
+    >
+      Text Editor
+    </Link>
+  </div>
+);
 
 export default ProductSelect;
