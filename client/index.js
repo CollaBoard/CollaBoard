b5f15d0234bdd118caf64da14155b0b540eaf5fc
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-08 17:14:13 -0600

    Correct linting errors in main client file

--- client/index.js
+++ client/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-import { promise } from 'redux-promise';
 import routes from './src/routes';
-import store from './src/data/store'
-
+import store from './src/data/store';
 
 
 ReactDOM.render(
-	<Provider store={ store }> 
-		<Router history={ browserHistory } routes={ routes } />
-	</Provider>
-	, document.getElementById('app')
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+  , document.getElementById('app')
 );
