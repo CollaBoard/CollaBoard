e37d5db02dfba6e6889275d4a600eec93b74ae50
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-08 17:22:38 -0600

    Correct linting errors in all client files

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,18 +5,15 @@ import App from './components/app';
 import Main from './components/main/';
 import Pricing from './components/pricing/';
 import Tutorials from './components/tutorials/';
-import Shop from './components/shop/';
 import Whiteboard from './components/whiteboard';
 import TextEditor from './components/text-editor';
 
 export default (
-	<Route path="/" component={App} >
-		<IndexRoute component={Main} />
-		<Route path="/pricing" component={Pricing} /> 
-		<Route path="/tutorials" component={Tutorials} /> 
-		<Route path="/shop" component={Shop} /> 
-		<Route path="/whiteboard" component={Whiteboard} /> 
-		<Route path="/texteditor" component={TextEditor} /> 
-
-	</Route> 
-)
+  <Route path="/" component={App} >
+    <IndexRoute component={Main} />
+    <Route path="/pricing" component={Pricing} />
+    <Route path="/tutorials" component={Tutorials} />
+    <Route path="/whiteboard" component={Whiteboard} />
+    <Route path="/texteditor" component={TextEditor} />
+  </Route>
+);
