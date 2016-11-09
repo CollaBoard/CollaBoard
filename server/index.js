26feb321e77b73e22621604592418860a47bff22
Chris <tranchris1994@yahoo.com>
2016-11-08 18:58:18 -0600

    Add socket.io-client to index.js, index.html, and package.json

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const socket = require('socket.io-client')('http://localhost');
+var socket = require('socket.io-client')('http://localhost');
 socket.on('connect', function() {});
 socket.on('event', function(data) {});
 socket.on('disconnect', function() {});
