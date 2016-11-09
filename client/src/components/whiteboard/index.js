7b0f2aaa108627d34db0b857ae50261bccacae31
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-11-09 12:19:44 -0600

    add second canvas, better xy positioning with getBoundingClientRect

--- client/src/components/whiteboard/index.js
+++ client/src/components/whiteboard/index.js
@@ -4,24 +4,42 @@ const Canvas = require('./Canvas');
 class Whiteboard extends React.Component {
   constructor(props) {
     super(props);
-    this.canvasLoaded = this.canvasLoaded.bind(this);
+    this.canvasLoaded1 = this.canvasLoaded1.bind(this);
+    this.canvasLoaded2 = this.canvasLoaded2.bind(this);
   }
 
 
-  canvasLoaded(canvas) {
-    this.canvas = new Canvas(canvas, {});
+  canvasLoaded1(canvas) {
+    this.canvas1 = new Canvas(canvas, {});
+  }
+
+  canvasLoaded2(canvas) {
+    this.canvas2 = new Canvas(canvas, {});
   }
 
   render() {
+    const style = { border: '1px solid #000' };
     return (
-      <canvas
-        ref={this.canvasLoaded}
-        id="whiteboard"
-        width="500"
-        height="500"
-      >
-        {'It appears your browser doesn\'t support HTML5 Canvas'}
-      </canvas>
+      <div>
+        <canvas
+          ref={this.canvasLoaded1}
+          id="whiteboard"
+          width="500"
+          height="500"
+          style={style}
+        >
+          {'It appears your browser doesn\'t support HTML5 Canvas'}
+        </canvas>
+        <canvas
+          ref={this.canvasLoaded2}
+          id="whiteboard2"
+          width="500"
+          height="500"
+          style={style}
+        >
+          {'It appears your browser doesn\'t support HTML5 Canvas'}
+        </canvas>
+      </div>
     );
   }
 }
