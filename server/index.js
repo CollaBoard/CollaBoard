ba6ab5e6b7b2f6f90a296c2e8c4b2747f58ab654
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-10 11:52:07 -0600

    Listen on server rather than app

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ const http = require('http');
 
 
 const app = express();
+const server = http.Server(app);
 // make socket listen
-socket.listen(http.Server(app));
+socket.listen(server);
 
 const routes = require('./routes');
 
@@ -27,8 +28,8 @@ app.use('/', routes);
 
 // Start the server!
 const port = process.env.PORT || 4000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
-module.exports = app;
+module.exports = server;
