2c809fe9a92060b7f387046b4229d1be00c05e7e
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-10 11:33:59 -0600

    Create a namespaced socket when creating a board

--- server/models/board.js
+++ server/models/board.js
@@ -1,24 +1,32 @@
 const Puid = require('puid');
 const db = require('../lib/knex-driver');
+const io = require('../socket').io;
 
 const puid = new Puid(false);
-
 const Board = module.exports;
 
 Board.find = function find(boardId) {
-  return db('shares').where('uid', boardId).limit(1)
+  return db('boards').where('uid', boardId).limit(1)
     .then((rows) => {
       if (!rows.length) {
-        return new Error(`Board ${boardId} was not found`);
+        throw new Error(`Board ${boardId} was not found`);
       }
+      // TODO: create the namespace if it doesn't exist already
       return rows[0];
     });
 };
 
-Board.create = function create() {
+Board.create = function create(type) {
   const uid = puid.generate();
-  return db('shares').insert({ uid }, 'uid')
-    .then(uids => uids[0])
+  return db('boards').insert({ uid, type })
+    .then(() => {
+      // TODO: move this into a constructor function of some sort
+      const socket = io.of(`/${uid}`);
+      socket.on('connection', () => {
+        console.log('Connection on ', uid);
+      });
+      return { uid, type };
+    })
     .catch((err) => {
       console.error(err);
       throw err;
