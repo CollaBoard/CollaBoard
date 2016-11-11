c3d462b415b7606afdf521d52b9d5819f76031f2
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-11 11:44:50 -0600

    Remove text editor listener from global socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -3,12 +3,8 @@ const socket = require('socket.io');
 let io;
 module.exports.listen = (http) => {
   io = socket.listen(http);
-  io.on('connection', (thisSocket) => {
+  io.on('connection', () => {
     console.log('Socket connected!');
-    thisSocket.on('text change', (newText) => {
-      console.log('sending new text:', newText);
-      thisSocket.broadcast.emit('serve text', newText);
-    });
   });
   module.exports.io = io;
 };
