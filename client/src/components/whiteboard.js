c4d7645962c6128965ab8888f8143b70f1f6ce7f
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-14 16:23:47 -0600

    Add temporary whiteboard fixes

--- client/src/components/whiteboard.js
+++ client/src/components/whiteboard.js
@@ -36,21 +36,21 @@ class Whiteboard extends React.Component {
         </canvas>
         <div className="circular-menu" id="context-menu">
           <div className="circle" id="circle">
-            <a onClick={() => { this.canvas.prop('color', 'red'); }}>
+            <a onClick={() => { this.state.canvas.prop('color', 'red'); }}>
               <i className="material-icons redBubble">lens</i></a>
-            <a onClick={() => { this.canvas.prop('color', 'orange'); }}>
+            <a onClick={() => { this.state.canvas.prop('color', 'orange'); }}>
               <i className="material-icons orangeBubble">lens</i></a>
-            <a onClick={() => { this.canvas.prop('color', 'yellow'); }}>
+            <a onClick={() => { this.state.canvas.prop('color', 'yellow'); }}>
               <i className="material-icons yellowBubble">lens</i></a>
-            <a onClick={() => { this.canvas.prop('color', 'green'); }}>
+            <a onClick={() => { this.state.canvas.prop('color', 'green'); }}>
               <i className="material-icons greenBubble">lens</i></a>
-            <a onClick={() => { this.canvas.prop('color', 'blue'); }}>
+            <a onClick={() => { this.state.canvas.prop('color', 'blue'); }}>
               <i className="material-icons blueBubble">lens</i></a>
-            <a onClick={() => { this.canvas.prop('color', 'purple'); }}>
+            <a onClick={() => { this.state.canvas.prop('color', 'purple'); }}>
               <i className="material-icons purpleBubble">lens</i></a>
-            <a onClick={() => { this.canvas.prop('color', 'black'); }}>
+            <a onClick={() => { this.state.canvas.prop('color', 'black'); }}>
               <i className="material-icons blackBubble">lens</i></a>
-            <a onClick={() => { this.canvas.prop('color', 'white'); }}>
+            <a onClick={() => { this.state.canvas.prop('color', 'white'); }}>
               <i className="material-icons whiteBubble">lens</i></a>
           </div>
         </div>
@@ -58,5 +58,11 @@ class Whiteboard extends React.Component {
     );
   }
 }
+Whiteboard.propTypes = {
+  socket: React.PropTypes.shape({
+    on: React.PropTypes.func.isRequired,
+    emit: React.PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Whiteboard;
