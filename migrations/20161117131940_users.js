3ce66672ae8aa4a546e7f4a3782594bb54570a40
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-18 12:14:22 -0600

    Add users and sessions tables

---
+++ migrations/20161117131940_users.js
@@ -0,0 +1,21 @@
+exports.up = function up(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.string('uid').primary();
+    table.string('google_id').unique();
+    table.string('github_id').unique();
+    table.string('email').nullable();
+    table.string('name').notNullable();
+    table.string('avatar').nullable();
+    table.timestamps(true, true);
+  })
+  .then(() => knex.schema.createTable('sessions', (table) => {
+    table.string('uid').primary();
+    table.string('token').notNullable();
+    table.string('user_uid').references('users.uid');
+    table.timestamp('expires', true);
+  }));
+};
+
+exports.down = function down(knex) {
+  return knex.schema.dropTable('sessions').then(() => knex.schema.dropTable('users'));
+};
