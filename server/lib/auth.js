7a1e5f4b546124c5b7e52c11523606894c02a985
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-19 14:08:44 -0600

    Add authentication middleware

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -1,67 +1,12 @@
-const cookieSession = require('cookie-session');
-const router = require('express').Router();
-const authport = require('authport');
-const User = require('../models/user');
+const helpers = require('./helpers');
 const Session = require('../models/session');
 
-authport.createServer({
-  service: 'github',
-  id: process.env.GITHUB_CLIENT_ID,
-  secret: process.env.GITHUB_CLIENT_SECRET,
-  scope: 'user:email',
-  redirect_uri: 'http://localhost:4000/auth/github/callback',
-});
-
-authport.createServer({
-  service: 'google',
-  id: `${process.env.GOOGLE_CLIENT_ID}.apps.googleusercontent.com`,
-  secret: process.env.GOOGLE_CLIENT_SECRET,
-  scope: ['profile', 'email'],
-  redirect_uri: 'http://localhost:4000/auth/google/callback',
-});
-
-authport.on('auth', (req, res, data) => {
-  const profile = data.data;
-  let findUser;
-  if (data.service === 'github') {
-    findUser = User.find({ github_id: profile.id })
-      .catch(() => new User({
-        github_id: profile.id,
-        email: profile.email,
-        name: profile.name || profile.login,
-        avatar: profile.avatar,
-      }).save());
-  } else if (data.service === 'google') {
-    findUser = User.find({ google_id: profile.id })
-      .catch(() => new User({
-        google_id: profile.id,
-        email: profile.email,
-        name: profile.name,
-        avatar: profile.picture,
-      }).save());
-  }
-  findUser
-    .then(user => new Session({ user_uid: user.uid, token: data.token }).save())
-    .then((session) => {
-      req.session.uid = session.uid;
-      res.redirect('/');
-    });
-});
-
-authport.on('error', (req, res, err) => {
-  res.send(err);
-});
-
-router.use(cookieSession({
-  name: 'collaboard_session',
-  secret: process.env.COOKIE_SECRET,
-  keys: ['secret_key_1', 'secret_key_2'],
-}));
-
-router.get('/auth/:service', authport.app);
-
-router.use('/api', (req, res, next) => {
-  if (!req.session) {
+const Auth = module.exports = {};
+Auth.checkLogin = isRequired => (req, res, next) => {
+  if (!req.session || !req.session.uid) {
+    if (isRequired) {
+      return next(new helpers.PermissionDenied('User must be logged in'));
+    }
     req.user = null;
     return next();
   }
@@ -70,6 +15,12 @@ router.use('/api', (req, res, next) => {
     .then((user) => {
       req.user = user;
       return next();
+    })
+    .catch(() => {
+      if (isRequired) {
+        return next(new helpers.PermissionDenied('User must be logged in'));
+      }
+      req.user = null;
+      return next();
     });
-});
-module.exports = router;
+};
