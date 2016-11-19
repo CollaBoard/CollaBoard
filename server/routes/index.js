453f16b52afa1807b800bac9abf1d760c99c954f
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-19 14:09:13 -0600

    Update authentication to new file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./api');
-const authentication = require('../lib/auth');
+const authentication = require('./auth');
 
 const router = express.Router();
 
