2822cd8bb4816a9c89689d169a99d2adcf3c5908
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-21 16:29:05 -0600

    Rework board model to work with new schema

--- server/models/board.js
+++ server/models/board.js
@@ -1,26 +1,90 @@
-const Puid = require('puid');
 const db = require('../lib/knex-driver');
 const Namespace = require('./namespace');
+const util = require('../lib/util');
 
-const puid = new Puid(false);
-const Board = module.exports;
 
-Board.find = function find(boardId) {
-  return db('boards').where('uid', boardId).limit(1)
+/**
+ * The Board constructor
+ * @param {object} [info={}] An options hash
+ * @param {string} info.name The title of the Board
+ * @param {string} [info.creator=null] The id of the team or user creating the board
+ * @param {string} [info.creator_type=null] The type of the creator ['user' or 'team']
+ * @param {boolean} [info.public=false] Whether this board should be publicly visible
+ * @param {boolean} [f=false]
+ */
+const Board = function Board(info = {}, f = false) {
+  let fetched = (f && info.uid) || false;
+  this.uid = fetched ? info.uid : util.uidShort();
+  this.name = info.name || null;
+  this.creator = info.creator || null;
+  this.creator_type = info.creator_type || null;
+  this.public = info.creator ? !!info.public : true;
+
+  // create a socket for this board if it dodesn't already exist
+  Namespace.create(this.uid);
+
+  this.save = () => (
+    (!fetched
+      ? db('boards').insert({
+        uid: this.uid,
+        name: this.name,
+        creator: this.creator,
+        creator_type: this.creator_type,
+        public: this.public,
+      })
+      : db('boards').where('uid', this.uid)
+        .update({
+          name: this.name,
+          public: !!this.public,
+        })
+    )
+      .then(() => {
+        fetched = true;
+        return this;
+      })
+      .catch(util.rethrow)
+  );
+};
+
+/**
+ * Find a board by ID
+ * @param {string} boardId
+ * @param {string} [userUid] The id of the user who is requesting the board
+ * @returns {Promise} Resolves to a Board
+ */
+Board.findById = Board.find = function find(boardUid, userUid) {
+  return db('boards').where('uid', boardUid).limit(1)
     .then((rows) => {
       if (!rows.length) {
-        throw new Error(`Board ${boardId} was not found`);
+        util.throwNotFound(`Board ${boardUid} was not found`);
       }
-
-      const board = rows[0];
-      // will only create namespace if it doesn't already exist
-      Namespace.create(board.uid);
-      return board;
+      const board = new Board(rows[0], true);
+      if (board.public || board.creator === userUid) {
+        return board;
+      }
+      if (!userUid) {
+        util.throwNotFound(`Board ${boardUid} was not found`);
+      }
+      return db('team_memberships')
+        .where(() => {
+          if (board.creator_type === 'team') {
+            return this.where('team_uid', board.creator);
+          }
+          this.where('user_uid', board.creator);
+        })
+        .andWhere('user_uid', userUid)
+        .then((results) => {
+          if (results.length) {
+            return board;
+          }
+          util.throwNotFound(`Board ${boardUid} was not found`);
+        })
+        .catch(util.rethrow);
     });
 };
 
 Board.create = function create(type) {
-  const uid = puid.generate();
+  const uid = util.uidShort();
   return db('boards').insert({ uid, type })
     .then(() => {
       // create a namespace for the new room
