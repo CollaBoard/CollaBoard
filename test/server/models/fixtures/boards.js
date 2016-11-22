8c2775cf229460730f061440926fb28e2848cdd1
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-22 17:35:26 -0600

    Create fixtures for users, teams, and boards

---
+++ test/server/models/fixtures/boards.js
@@ -0,0 +1,9 @@
+module.exports = {
+  basic: {},
+  named1: {
+    name: 'Named Board 1',
+  },
+  named2: {
+    name: 'Named Board 2',
+  },
+};
---
+++ test/server/models/fixtures/teams.js
@@ -0,0 +1,9 @@
+module.exports = {
+  uid: '123e4567-e89b-12d3-a456-426655440000',
+  name: 'Test team 1!',
+  avatar: 'https://robohash.org/testTeam1',
+  member_count: 0,
+  board_count: 0,
+  boards: [],
+  members: [],
+};
