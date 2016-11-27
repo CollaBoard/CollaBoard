f4e024d13b2f580a243ec6908cff4853ccd60b84
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-26 18:40:53 -0600

    Add scss compiler

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
 }
 /* eslint-enable */
 
+const http = require('http');
 const browserify = require('browserify-middleware');
 const express = require('express');
 const bodyParser = require('body-parser');
-const http = require('http');
-const sassMiddleware = require('node-sass-middleware');
+const scssMiddleware = require('./routes/scss');
 
 const socket = require('./socket');
 
@@ -30,11 +30,9 @@ app.use('/app-bundle.js', browserify('./client/src/index.js', {
   ],
 }));
 
-app.use(sassMiddleware({
-  src: './client/src/scss/app.scss',
-  dest: 'client/public/assets/main.css',
+app.get('/assets/main.css', scssMiddleware({
+  file: './client/src/scss/app.scss',
   outputStyle: 'compressed',
-  sourceMap: 'true',
 }));
 
 
