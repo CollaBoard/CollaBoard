76e80e8984ac43e25aad70170878de429a8cfa9e
Alex Mitchell <shamsup@users.noreply.github.com>
2016-11-26 20:52:24 -0600

    Remove redux-logger

--- client/src/data/store.js
+++ client/src/data/store.js
@@ -1,21 +1,7 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
-/* eslint-disable */
-// require redux-logger only in development mode
-let finalCreateStore;
-// if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
-//   console.log('NODE_ENV: ', process.env.NODE_ENV);
-//   const logger = require('redux-logger');
-//   finalCreateStore = compose(
-//     applyMiddleware(logger())
-//   )(createStore);
-// } else {
-  finalCreateStore = compose(
-    applyMiddleware()
-  )(createStore);
-// }
-/* eslint-enable */
 
+const finalCreateStore = compose(applyMiddleware())(createStore);
 
 export default function configureStore(initialState = {}) {
   return finalCreateStore(rootReducer, initialState);
